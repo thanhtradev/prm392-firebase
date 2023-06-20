d4098ef137d32fc2b6fd9f13e789328d785dc1b4
thanhtradev <thanhtra2001.stt@gmail.com>
2023-06-21 02:50:33 +0700

    init

---
+++ app/src/main/java/com/prm/firebase/MainActivity.java
@@ -0,0 +1,44 @@
+package com.prm.firebase;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.messaging.FirebaseMessaging;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        TextView instructionTextView = findViewById(R.id.instruction_text_view);
+        TextView tokenTextView = findViewById(R.id.token_text_view);
+        instructionTextView.setText("Please put the app into the background by pressing the home button.");
+
+        FirebaseMessaging.getInstance().getToken()
+                .addOnCompleteListener(new OnCompleteListener<String>() {
+                    @Override
+                    public void onComplete(@NonNull Task<String> task) {
+                        if (!task.isSuccessful()) {
+                            System.out.println("Fetching FCM registration token failed" + task.getException());
+                            return;
+                        }
+
+                        // Get new FCM registration token
+                        String token = task.getResult();
+
+                        // Log and toast
+//                        String msg = getString(R.string.msg_token_fmt, token);
+                        System.out.println(token);
+                        tokenTextView.setText("Your token: " + token);
+                        Toast.makeText(MainActivity.this, token, Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
+}
